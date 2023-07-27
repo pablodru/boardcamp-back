5ae654189f5e34bedd6f586cc0d75fbfc03bb5f2
pablodru <pdrocha03@gmail.com>
2023-07-27 17:10:45 -0300

    feat: CRUD de games

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -1,5 +1,13 @@
+import { db } from "../database/database.connection.js";
+
 export async function getGames(req, res) {
     try {
+
+        const games = await db.query(
+            `SELECT * FROM games;`
+        )
+        
+        res.status(200).send(games.rows)
         
     } catch (err) {
         res.status(500).send(err.message);
@@ -7,7 +15,21 @@ export async function getGames(req, res) {
 }
 
 export async function newGame(req, res) {
+    const { name, image, stockTotal, pricePerDay } = req.body;
+
     try {
+
+        const existingGame = await db.query(
+            `SELECT name FROM games WHERE name=$1;`, [name]
+        );
+
+        if ( existingGame.rows.length > 0 ) return res.sendStatus(409);
+
+        await db.query(
+            `INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay]
+            )
+
+        res.sendStatus(201);
 
     } catch (err) {
         res.status(500).send(err.message);
--- src/schemas/clients.schemas.js
+++ src/schemas/clients.schemas.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 export const clientsSchema = joi.object({
     name: joi.string().trim().required(),
-    phone: joi.string().pattern(/^\d+$/).length(10,11).required(),
-    cpf: joi.string().pattern(/^\d+$/).length(11).required(),
+    phone: joi.string().min(10).max(11).required().regex(/^\d+$/),
+    cpf: joi.string().length(11).required().regex(/^\d+$/),
     birthday: joi.date().iso().required()
 });
